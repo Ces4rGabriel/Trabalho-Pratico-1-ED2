02e34c95394c71e50f1160337afde19702b88fd6
Pedro <pedro.drummond33@gmail.com>
2023-01-30 16:14:39 -0300

    m

---
+++ analise.h
@@ -0,0 +1,11 @@
+#ifndef ANALISE_H
+#define ANALISE_H
+typedef struct{
+    double tempo;
+    double tempoC;
+    int comparacoes, comparacoesC;
+    int nTransferencias;
+    int criacao;
+}Analis;
+
+#endif
